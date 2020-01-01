ad00159d3768c1573bb574b885fd04f6287538c1
Islahul <izunjani@mavenoid.com>
2020-01-01 20:04:21 +0100

    More cleanup

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -11,10 +11,10 @@ import Layout from '../components/Layout'
 export const IndexPageTemplate = ({
   image,
   title,
-  heading,
-  subheading,
+  // heading,
+  // subheading,
   mainpitch,
-  description,
+  // description,
   // intro,
 }) => (
   <div>
@@ -51,19 +51,6 @@ export const IndexPageTemplate = ({
         >
           {title}
         </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {subheading}
-        </h3>
       </div>
     </div>
     <section className="section section--gradient">
@@ -76,18 +63,18 @@ export const IndexPageTemplate = ({
                   <div className="tile">
                     <h1 className="title">{mainpitch.title}</h1>
                   </div>
-                  {/* <div className="tile">
+                  <div className="tile">
                     <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div> */}
+                  </div>
                 </div>
-                <div className="columns">
+                {/* <div className="columns">
                   <div className="column is-12">
                     <h3 className="has-text-weight-semibold is-size-2">
                       {heading}
                     </h3>
                     <p>{description}</p>
                   </div>
-                </div>
+                </div> */}
                 {/* <Features gridItems={intro.blurbs} /> */}
                 {/* <div className="columns">
                   <div className="column is-12 has-text-centered">
@@ -120,12 +107,7 @@ IndexPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   title: PropTypes.string,
   heading: PropTypes.string,
-  subheading: PropTypes.string,
   mainpitch: PropTypes.object,
-  description: PropTypes.string,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
 }
 
 const IndexPage = ({ data }) => {
@@ -137,10 +119,7 @@ const IndexPage = ({ data }) => {
         image={frontmatter.image}
         title={frontmatter.title}
         heading={frontmatter.heading}
-        subheading={frontmatter.subheading}
         mainpitch={frontmatter.mainpitch}
-        description={frontmatter.description}
-        intro={frontmatter.intro}
       />
     </Layout>
   )
@@ -169,12 +148,10 @@ export const pageQuery = graphql`
           }
         }
         heading
-        subheading
         mainpitch {
           title
           description
         }
-        description
       }
     }
   }
